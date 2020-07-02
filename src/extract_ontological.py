e046e2e846b0ac47b474e9411fb82487e561e5ce
irina-kurtz <irina.kurtz@omniseq.com>
2020-07-02 14:51:59 -0400

    in progress, do not attempt to run

--- src/extract_ontological.py
+++ src/extract_ontological.py
@@ -1,40 +1,15 @@
 """
 This script extracts data from GO_diseases files and converts
-the files to SQL tables:
-1. combine_files - extracts data from json  files to a dataframe
-2. parse_go_main - parses and cleans dataframe
-3. write_load_files - writes dataframe to csv file
-4. get_schema - gets the schema for SQL table
-5. write_sql - writes sql table
-
-6/3/2020
+the files to a dataframe that contains data needed for ontological disease table:
+7/2/2020
 by IK
 """
-import mysql.connector
-import create_id
+
 import datetime
 import pandas
 from pandas.io.json import json_normalize
-import write_load_files
-import create_editable_statement
-import create_EditableStringList
-import write_sql
-import create_EditableXrefsList
-
-editable_statement_list = ['name', 'definition']
-editable_synonyms_list = ['synonyms']
-loader_id = '007'
-load_directory = 'C:/Users/irina.kurtz/PycharmProjects/Manuel/writeDiseases/load_files/'
-table_name = 'GoDiseases'
-id_class = create_id.ID('', '')
-
-
-import csv
-from sql_utils import load_table, create_table, does_table_exist, \
-    get_local_db_connection, maybe_create_and_select_database, \
-    drop_table_if_exists
 import json
-import get_schema
+
 
 def parse_go():
     print(datetime.datetime.now().strftime("%H:%M:%S"))
@@ -49,8 +24,21 @@ def parse_go():
     df=create_go_dataframe(files)
     return df
 
-def create_go_dataframe( files):
+# Add diseases extracted from GO files to a ontological disease dataframe
+def add_disease_to_df(code_list, df_to_edit, index, substring, id_string, column_name):
+    diseases = [l for l in code_list if substring in l]
+    if diseases:
+        final_disease_list = []
+        for disease in diseases:
+            disease_id = id_string + disease.replace(substring, '')
+            final_disease_list.append(disease_id)
+        df_to_edit.at[index, column_name] = final_disease_list
+    else:
+        df_to_edit.at[index, column_name] = diseases
+    return df_to_edit
 
+# Converts files to dataframes and combines dataframes
+def create_go_dataframe( files):
     dataframe_list = list_of_dfs_to_combine(files)
     go_df = combine_dataframes(dataframe_list)
     return go_df
@@ -66,6 +54,7 @@ def list_of_dfs_to_combine(files):
         dataframe_list.append(df)
     return dataframe_list
 
+# Combines dataframes
 def combine_dataframes(dataframe_list):
     # Clean dataframes and add graph_id column
     dfs_to_combine = []
@@ -80,37 +69,22 @@ def combine_dataframes(dataframe_list):
             df.at[index, 'goDiseases'] = ''
             df.at[index, 'oncoTreeDiseases'] = ''
 
+            # Get diseases from 'codes' column
             code_list = row[ 'codes']
             if type(code_list) is list:
                 if code_list:
-                    jax = [j for j in code_list if 'JAX-CKB=' in j]
-                    if jax:
-                        jax_list = []
-                        for jax_disease in jax:
-                            jax_id = 'jax_disease_' +  jax_disease.replace('JAX-CKB=JAX', '')
-                            jax_list.append(jax_id)
-                        df.at[index, 'jaxDiseases'] = jax_list
-                    else:
-                        df.at[index, 'jaxDiseases'] = jax
-
-                do = [d for d in code_list if 'DOID=' in d]
-                if do:
-                    do_list = []
-                    for do_disease in do:
-                        do_id = 'do_disease_' + do_disease.replace('DOID=', '')
-                        do_list.append(do_id)
-                    df.at[index, 'doDiseases'] = do_list
-                else:
-                    df.at[index, 'doDiseases'] = do
-
+                    df = add_disease_to_df(code_list, df, index, 'JAX-CKB=JAX', 'jax_disease_', 'jaxDiseases')
+                    df = add_disease_to_df(code_list, df, index, 'DOID=', 'do_disease_', 'doDiseases')
             else:
                 temp_list = []
                 df.at[index, 'jaxDiseases'] = temp_list
                 df.at[index, 'doDiseases'] = temp_list
 
+            # Assign ontological_disease id
             df.at[index, 'graph_id'] = 'ontological_disease_' + str(counter)
             counter += 1
 
+            # Get info rom oncotree column
             oncotree_code = row['oncotree_code']
             if pandas.isnull(oncotree_code):
                 onco_list = []
@@ -118,17 +92,14 @@ def combine_dataframes(dataframe_list):
             else:
                 if type(oncotree_code) is list:
                     if oncotree_code:
-                        onco_list = []
-                        for onco_disease  in oncotree_code:
-                            onco_id = 'oncotree_disease_' + onco_disease
-                            onco_list.append(onco_id)
-                            df.at[index, 'oncoTreeDiseases'] = onco_list
+                        df = add_disease_to_df(oncotree_code, df, index, '', 'oncotree_disease_', 'oncoTreeDiseases')
                 else:
                     onco_list = []
                     onco_id = 'oncotree_disease_' + oncotree_code
                     onco_list.append(onco_id)
                     df.at[index, 'oncoTreeDiseases'] = onco_list
 
+            # Assign go_disease id (using go diseases as data for ontological diseases table)
             go_id = 'go_disease_' + row[ 'id']
             go_list = []
             go_list.append(go_id)
