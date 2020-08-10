242cecab6d5fef6cf8c6dd70359dd602985f4b7a
irina-kurtz <irina.kurtz@omniseq.com>
2020-08-10 16:15:47 -0400

    ...

--- src/write_diseases.py
+++ src/write_diseases.py
@@ -13,6 +13,10 @@ import stat
 load_directory = '../load_files/'
 loader_id = 'user_20200422163431232329'
 id_class = create_id.ID('', '', 0, 0, 0, 0, 0, 0, 0, True)
+files_with_tags = ['EditableStringList', 'EditableStringListElements', 'EditableStatement',
+                   'EditableXrefsList', 'EditableXrefsListElements', 'Xref', 'EditableBoolean',
+                   'Author', 'Journal', 'EditableStatement_LiteratureReference', 'LiteratureReference',
+                   'LiteratureReference_Author', 'EditableStatement_InternetReference']
 
 def main(load_directory, loader_id, id_class):
     create_do_disease_db.main(load_directory, loader_id, id_class)
@@ -23,6 +27,9 @@ def main(load_directory, loader_id, id_class):
     create_tcode_db.main(load_directory, loader_id, id_class)
     create_mcode_db.main(load_directory, loader_id, id_class)
     create_ontological_disease_db.main(load_directory, loader_id, id_class)
+
+    for file in files_with_tags:
+        os.rename(r'../load_files/' + file + '.csv', r'../load_files/' + file  + '_ik' + '.csv')
     print('All diseases are loaded, thank you!')
 
 if __name__ == "__main__":
